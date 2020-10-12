490253c7f86e9f28aa48ec6b510e78751a536258
irina777-create <71390012+irina777-create@users.noreply.github.com>
2020-10-12 22:07:02 +0300

    Update main.js
    Написала код в файле main.js

---
+++ js/main.js
@@ -0,0 +1,39 @@
+"use stritc"
+var photos=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+photos.forEach(function(src,description,likes,comments){
+  console.log(src);
+  console.log(description);
+  console.log(likes);
+  console.log(comments);
+});
+var comments={
+  avatar:"img/avatar-4.svg",
+  message:"Все отлично!",
+  name:"Коля"
+}
+var comments= document.guerySelectorAll('.picture 4 comments');
+
+var comments={
+  avatar:"img/avatar-6.svg",
+  message:"Моя бабушка случайно чихнула с фотоаппаратом в руках и у нее получилась фотография лучше",
+  name:"Миша"
+}
+var comments= document.guerySelectorAll('.picture 6 comments');
+
+var comments={
+  avatar:"img/avatar-10.svg",
+  message:"Лица у людей на фотке перекошены, как будто их избивают.Как можно было поймать такой неудачный момент?!",
+  name:"Андрей"
+}
+var comments= document.guerySelectorAll('.picture 10 comments');
+
+var likes=[15];
+var likes=document.guerySelectorAll('.picture 15 likes');
+
+var likes=[20];
+var likes=document.guerySelectorAll('.picture 20 likes');
+
+
+
+
+
